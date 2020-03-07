5716f7278f65e4dee602d4b756db685ed2eb4f1b
Artem <aopoltorzhicky@gmail.com>
2020-03-08 00:11:40 +0300

    BigMap Viewer beautify

--- src/views/BigMapViewer.vue
+++ src/views/BigMapViewer.vue
@@ -1,23 +1,28 @@
 <template>
-  <div class="elevation-1 white">
+  <div class="elevation-0 white">
     <v-toolbar flat>
       <div>
         <v-toolbar-title>Big Map Viewer</v-toolbar-title>
-        <span class="grey--text hash">{{ $route.params.address }} (Ptr {{ $route.params.ptr }})</span>
+        <span class="grey--text hash">
+          <router-link
+            :to="{name: 'project', params:{address: $route.params.address, network: $route.params.network}}"
+          >{{ $route.params.address }}</router-link>
+          (Ptr {{ $route.params.ptr }})
+        </span>
       </div>
       <v-spacer></v-spacer>
       <v-text-field
         v-model="search"
         append-icon="mdi-magnify"
-        label="Search"
+        label="Search by key hash or key value"
         single-line
         hide-details
       ></v-text-field>
     </v-toolbar>
     <v-row>
-      <v-col class="6">
+      <v-col cols="6">
         <v-data-table
-          class="px-4"
+          class="elevation-2 ml-4 pa-2"
           :loading="loading"
           loading-text="Loading... Please wait"
           :items="bigmap"
@@ -28,66 +33,89 @@
           :custom-filter="fileterItems"
           @click:row="showBigMapDiffDetails"
         >
-          <template v-slot:item.key="{ item }">
-            <div style="cursor:pointer">
-              <span class="overline ml-3">Last action at {{ item.data.level }} level</span>
-              <v-treeview :items="getTree(item.data.key)" open-all transition class="storage">
-                <template v-slot:label="{ item }">
-                  <span>{{ item.name }}:</span>&nbsp;
-                  <span :class="item.type">{{ item.value }}</span>
-                </template>
-              </v-treeview>
-            </div>
+          <template v-slot:item="{item}">
+            <tr
+              :class="isActive(item)"
+              @click="showBigMapDiffDetails(item)"
+              :key="item.data.key_hash"
+            >
+              <td class="px-2">
+                <div style="cursor:pointer">
+                  <span class="overline ml-4">Last action at {{ item.data.level }} level</span>
+                  <v-treeview :items="getTree(item.data.key)" open-all transition class="storage">
+                    <template v-slot:label="{ item }">
+                      <span>{{ item.name }}:</span>&nbsp;
+                      <span :class="item.type">{{ item.value }}</span>
+                    </template>
+                  </v-treeview>
+                </div>
+              </td>
+            </tr>
           </template>
         </v-data-table>
       </v-col>
-      <v-col sm="auto">
-        <v-divider vertical></v-divider>
-      </v-col>
-      <v-col cols="5">
-        <v-card :elevation="0" v-if="selectedBigMapDiff">
+      <v-col cols="6">
+        <v-card :elevation="2" v-if="selectedBigMapDiff" class="mr-4 pa-2">
           <v-card-title class="hash key-hash">{{ selectedBigMapDiff.key_hash }}</v-card-title>
           <v-card-text>
             <div class="bmd-value">
               <span class="overline ml-3">Value at {{ selectedBigMapDiff.level }} level</span>
-              <v-treeview
-                :items="getTree(selectedBigMapDiff.value)"
-                open-all
-                transition
-                class="storage"
-                v-if="selectedBigMapDiff.value !== null"
-              >
-                <template v-slot:label="{ item }">
-                  <span>{{ item.name }}:</span>&nbsp;
-                  <span :class="item.type">{{ item.value }}</span>
-                </template>
-              </v-treeview>
+              <div v-if="selectedBigMapDiff.value !== null">
+                <v-treeview
+                  :items="getTree(selectedBigMapDiff.value)"
+                  hoverable
+                  open-all
+                  transition
+                  activatable
+                  open-on-click
+                  return-object
+                  class="storage"
+                  :active.sync="activeField"
+                >
+                  <template v-slot:label="{ item }">
+                    <span>{{ item.name }}:</span>&nbsp;
+                    <span :class="item.type">{{ item.value }}</span>
+                  </template>
+                </v-treeview>
+              </div>
               <p v-else class="ml-3 mb-0">Has been removed</p>
             </div>
-            <div v-if="actions.length > 0" class="mt-7">
-              <v-subheader>Actions</v-subheader>
+            <div v-if="actions.length > 0" class="mt-4">
               <v-list>
-                <template v-for="(item, idx) in actions">
-                  <v-list-item :key="idx" @click="selectedBigMapDiff = item">
-                    <v-list-item-content>
-                      <v-list-item-subtitle>At {{ item.level }} level</v-list-item-subtitle>
-                    </v-list-item-content>
-                    <v-list-item-action>
-                      <v-list-item-action-text class="red--text" v-if="item.value === null">remove</v-list-item-action-text>
-                      <v-list-item-action-text class="primary--text" v-else>update</v-list-item-action-text>
-                    </v-list-item-action>
-                  </v-list-item>
-                  <v-divider :key="actions.length + idx" v-if="idx < actions.length -1" />
-                </template>
+                <v-list-item-group v-model="selectedAction" active-class="primary--text">
+                  <template v-for="(item, idx) in actions">
+                    <v-list-item :key="idx">
+                      <v-list-item-content>
+                        <v-list-item-subtitle>At {{ item.level }} level</v-list-item-subtitle>
+                      </v-list-item-content>
+                      <v-list-item-action>
+                        <v-list-item-action-text class="red--text" v-if="item.value === null">remove</v-list-item-action-text>
+                        <v-list-item-action-text class="primary--text" v-else>update</v-list-item-action-text>
+                      </v-list-item-action>
+                    </v-list-item>
+                    <v-divider :key="actions.length + idx" v-if="idx < actions.length -1" />
+                  </template>
+                </v-list-item-group>
               </v-list>
             </div>
           </v-card-text>
+          <v-fade-transition>
+            <v-overlay v-if="loadingDetails" absolute>
+              <v-progress-circular indeterminate size="64"></v-progress-circular>
+            </v-overlay>
+          </v-fade-transition>
         </v-card>
-        <v-card v-else :elevation="0">
-          <v-card-text>Select one of big map items</v-card-text>
+        <v-card
+          v-else
+          :elevation="0"
+          class="d-flex flex-column align-center justify-center mt-12 transparent"
+        >
+          <v-icon size="100" color="grey">mdi-code-braces</v-icon>
+          <span class="headline grey--text">Select one of big map items</span>
         </v-card>
       </v-col>
     </v-row>
+    <TreeNodeDetails v-model="showTreeNodeDetails" :data="active" v-if="active" />
   </div>
 </template>
 
@@ -97,14 +125,22 @@ import { mapActions } from "vuex";
 import { getContractBigMap, getContractBigMapByKeyHash } from "@/api/index.js";
 import { getTree } from "@/utils/tree.js";
 
+import TreeNodeDetails from "@/components/TreeNodeDetails.vue";
+
 export default {
   name: "BigMapViewer",
+  components: {
+    TreeNodeDetails
+  },
   data: () => ({
     loading: true,
     loadingDetails: true,
+    showTreeNodeDetails: false,
+    activeField: [],
     search: "",
     bigmap: [],
     selectedBigMapDiff: null,
+    selectedAction: 0,
     actions: []
   }),
   computed: {
@@ -116,6 +152,12 @@ export default {
           sortable: false
         }
       ];
+    },
+    active() {
+      if (this.activeField.length > 0) {
+        return this.activeField[0];
+      }
+      return null;
     }
   },
   created() {
@@ -145,7 +187,9 @@ export default {
         this.$route.params.ptr,
         this.selectedBigMapDiff.key_hash
       )
-        .then(res => (this.actions = res))
+        .then(res => {
+          this.actions = res;
+        })
         .catch(err => {
           console.log(err);
           this.showError(err);
@@ -162,18 +206,29 @@ export default {
     fileterItems(value, search, item) {
       if (item.data.key_hash.includes(search)) return true;
       if (item.datakey !== null) {
-        let key = JSON.stringify(item.data.key);
-        if (key.includes(search)) return true;
-      }
-      if (item.data.value !== null) {
-        let val = JSON.stringify(item.data.value);
-        if (val.includes(search)) return true;
+        let key = JSON.stringify(item.data.key).toLowerCase();
+        if (key.includes(search.toLowerCase())) return true;
       }
       return false;
+    },
+    isActive(item) {
+      if (
+        this.selectedBigMapDiff &&
+        item.data &&
+        this.selectedBigMapDiff.key_hash === item.data.key_hash
+      )
+        return "filled-row";
+      return "";
     }
   },
   watch: {
-    $route: "requestData"
+    $route: "requestData",
+    selectedAction(newValue) {
+      this.selectedBigMapDiff = this.actions[newValue];
+    },
+    active() {
+      this.showTreeNodeDetails = !this.showTreeNodeDetails;
+    }
   }
 };
 </script>
@@ -190,6 +245,10 @@ export default {
   .object {
     color: #bbb;
   }
+}
+
+.filled-row {
+  background-color: #dcedc8;
 }
 
 .bmd-value {
