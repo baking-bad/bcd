7b1ce037ad033d7011794a042c0a9cc0eea7c10c
m-kus <mz@baking-bad.org>
2020-06-10 14:22:27 +0300

    redirect to dashboard after login

--- src/views/home/Welcome.vue
+++ src/views/home/Welcome.vue
@@ -8,7 +8,7 @@ import { login } from '@/utils/auth.js';
 export default {
   created() {
     login(this.$route.query.jwt);
-    this.$router.push({ path: "/" });
+    this.$router.push({ path: "/dashboard" });
   }
 };
 </script>
