4bc7c007fe310eafa96b26db47471e8a36464587
Artem <aopoltorzhicky@gmail.com>
2020-08-10 20:30:18 +0300

    View: transfers

--- src/api/bcd.js
+++ src/api/bcd.js
@@ -663,9 +663,9 @@ export class BetterCallApi {
       })
   }
 
-  getTokenVolumeSeries(network, period, address, token_id) {
+  getTokenVolumeSeries(network, period, addresses, token_id) {
     let params = [];
-    params.push(`address=${address}`)
+    params.push(`contracts=${addresses.join(',')}`)
 
     params.push(`period=${period}`)
 
