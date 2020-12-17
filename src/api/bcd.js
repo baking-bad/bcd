ec7340c3283cd52810e0aa2781eabed39ba44f1c
Roman Serikov <roman.serikov@yandex.ru>
2020-12-17 17:31:36 +0300

    fix tezos client generate command string

--- src/api/bcd.js
+++ src/api/bcd.js
@@ -282,7 +282,7 @@ export class BetterCallApi {
         if (res.status != 200) {
           throw new RequestFailedError(res);
         }
-        return res.data;
+        return res.request.response;
       })
   }
 
