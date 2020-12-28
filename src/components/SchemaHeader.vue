d9024043afdd45ee9e801e1dd4f8cc045e13ceff
daniilorain <dnlorn@gmail.com>
2020-12-28 16:27:56 +0300

    fix refactoring problems for SchemaHeader

--- src/components/SchemaHeader.vue
+++ src/components/SchemaHeader.vue
@@ -1,14 +1,20 @@
 <template>
-  <h2 class="ml-6 font-weight-regular" v-if="title">
+  <h2 class="ml-6 font-weight-regular">
     <span class="mr-2 hash font-weight-thin">{{ title }}</span>
-    <span v-if="isStorage" class="secondary--text" v-html="helpers.shortcut(name)"></span>
-    <span v-else class="hash secondary--text">{{ name }}</span>
+    <span v-if="isStorage" class="secondary--text" v-html="storageHtml"></span>
+    <span v-else class="hash secondary--text">{{ storageName }}</span>
   </h2>
 </template>
 
 <script>
 export default {
-name: "SchemaHeader"
+  name: "SchemaHeader",
+  props: {
+    isStorage: Boolean,
+    storageHtml: String,
+    storageName: String,
+    title: String,
+  }
 }
 </script>
 
