7477aaf05166e8e286f0b0d5cec1c7a5da9943e5
daniilorain <dnlorn@gmail.com>
2021-01-11 14:35:21 +0300

    refactor transfer tab

---
+++ src/views/contract/TransfersTab/TransfersTab.vue
@@ -0,0 +1,60 @@
+<template>
+  <v-container class="canvas fill-canvas pa-8 ma-0" fluid>
+    <v-row v-if="balances" no-gutters>
+      <v-col cols="9" class="pa-2">
+        <TransferList
+          :address="address"
+          :network="network"
+          :token="token"
+          :contract="token.contract"
+        />
+      </v-col>
+      <v-col cols="3" class="pa-2">
+        <TransfersCard
+          @selectedToken="updateSelectedToken"
+          :balances="balances"
+        />
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+import TransferList from "@/views/contract/TransferList.vue";
+import TransfersCard from "@/views/contract/TransfersTab/TransfersCard";
+
+export default {
+  name: "TransfersTab",
+  props: {
+    contract: Object,
+    network: String,
+    address: String,
+  },
+  components: {
+    TransfersCard,
+    TransferList,
+  },
+  computed: {
+    balances() {
+      if (!this.contract) return [];
+      return this.contract.tokens;
+    },
+    token() {
+      if (
+        this.selectedToken < 0 ||
+        this.selectedToken >= this.contract.tokens.length
+      )
+        return null;
+      return this.contract.tokens[this.selectedToken];
+    },
+  },
+  methods: {
+    updateSelectedToken(newVal) {
+      this.selectedToken = newVal
+    },
+  },
+  data: () => ({
+    selectedToken: 0,
+  }),
+};
+</script>
