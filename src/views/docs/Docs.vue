cb7857609519d7507226356aa3e981f7a11e63d2
daniilorain <dnlorn@gmail.com>
2021-01-12 01:39:40 +0300

    add docs route

---
+++ src/views/docs/Docs.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="fill-height canvas">
+    <SideNavigation :app="true" />
+  </div>
+</template>
+
+<script>
+import SideNavigation from "@/components/SideNavigation.vue";
+
+export default {
+  name: "Diff",
+  components: {
+    SideNavigation,
+  },
+};
+</script>
