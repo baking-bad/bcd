a3d17fd220bd1f9e42d7a2893bcbc200398f0c0f
daniilorain <dnlorn@gmail.com>
2021-01-12 13:38:41 +0300

    add redoc-wrapper

--- src/views/docs/Docs.vue
+++ src/views/docs/Docs.vue
@@ -1,16 +1,33 @@
 <template>
-  <div class="fill-height canvas">
-    <SideNavigation :app="true" />
-  </div>
+  <redoc-wrapper :spec-or-spec-url="'http://localhost:14000/v1/swagger.json'" :options="redocOptions"/>
 </template>
 
 <script>
-import SideNavigation from "@/components/SideNavigation.vue";
+import RedocWrapper from '@hnluu8/vue-redoc-wrapper'
 
 export default {
   name: "Diff",
   components: {
-    SideNavigation,
+    RedocWrapper
   },
+  data() {
+    return {
+      redocOptions: {
+        scrollYOffset: 0,
+      },
+    }
+  }
 };
 </script>
+
+<style lang="scss" scoped>
+#redoc-container {
+  .menu-content {
+    top: 0 !important;
+    height: 100% !important;
+  }
+  .api-content {
+    background: #FAFAFA;
+  }
+}
+</style>
