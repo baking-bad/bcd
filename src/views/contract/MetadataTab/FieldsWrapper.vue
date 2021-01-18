dddefa997fe7d5891872f9ef9e61bba8a073b280
daniilorain <dnlorn@gmail.com>
2021-01-18 20:02:51 +0300

    change reserved fields size

--- src/views/contract/MetadataTab/FieldsWrapper.vue
+++ src/views/contract/MetadataTab/FieldsWrapper.vue
@@ -14,21 +14,15 @@
         selectable
     >
       <v-list-item-content class="pt-0 pb-0">
-        <v-list-item-title class="overline d-flex">
+        <v-list-item-title class="d-flex">
           <span>
             {{
               field.key
             }}:
           </span>
-          <span class="grey--text ml-3" v-if="typeof field.value !== 'object'">
+          <span class="grey--text ml-3">
             {{ field.value }}
           </span>
-          <vue-json-pretty
-              v-else
-              class="ml-3"
-              :data="field.value"
-              :deep="0"
-          />
         </v-list-item-title>
       </v-list-item-content>
     </v-list-item>
@@ -59,6 +53,11 @@ export default {
 .fields-wrapper {
   .v-list-item {
     min-height: 32px;
+    &__title {
+      span {
+        font-size: 0.75rem;
+      }
+    }
   }
   .vjs-tree {
     font-size: 0.75rem;
