098d591a3c9b0d8c3075bd03d9ce6437564c3032
Roman Serikov <roman.serikov@yandex.ru>
2021-01-27 20:01:06 +0300

    Fix OAuth link

--- src/views/home/HomeToolbar.vue
+++ src/views/home/HomeToolbar.vue
@@ -1,6 +1,11 @@
 <template>
   <v-app-bar app clipped-left flat class="canvas pr-4">
-    <v-menu open-on-hover bottom offset-y v-if="!isAuthorized && config.oauth_enabled">
+    <v-menu
+      open-on-hover
+      bottom
+      offset-y
+      v-if="!isAuthorized && config.oauth_enabled"
+    >
       <template v-slot:activator="{ on }">
         <v-btn color="primary" text small v-on="on">
           <v-icon class="mr-1" small>mdi-login</v-icon>Sign in
@@ -10,7 +15,7 @@
       <v-list class="pa-0">
         <v-list-item
           class="d-flex align-center px-4"
-          :href="`${config.API_URI}oauth/github/login`"
+          :href="`${config.API_URI}/oauth/github/login`"
         >
           <v-list-item-title>
             <v-icon class="mr-4">mdi-github</v-icon>GitHub
@@ -18,7 +23,7 @@
         </v-list-item>
         <v-list-item
           class="d-flex align-center px-4"
-          :href="`${config.API_URI}oauth/gitlab/login`"
+          :href="`${config.API_URI}/oauth/gitlab/login`"
         >
           <v-list-item-title>
             <v-icon class="mr-4">mdi-gitlab</v-icon>GitLab
@@ -29,7 +34,13 @@
 
     <v-tooltip bottom v-else-if="profile != null">
       <template v-slot:activator="{ on }">
-        <v-btn v-on="on" :to="{name: 'dashboard'}" fab small class="ml-2 mr-4">
+        <v-btn
+          v-on="on"
+          :to="{ name: 'dashboard' }"
+          fab
+          small
+          class="ml-2 mr-4"
+        >
           <v-avatar size="40">
             <img :src="profile.avatar_url" :alt="profile.login" />
           </v-avatar>
@@ -96,14 +107,14 @@ export default {
     },
     apiDocsUrl() {
       return `${window.location.origin}/docs`;
-    }
+    },
   },
   methods: {
     toggleTheme() {
       this.$vuetify.theme.dark = !this.$vuetify.theme.dark;
       localStorage.setItem("dark", this.$vuetify.theme.dark);
-    }
-  }
+    },
+  },
 };
 </script>
 
