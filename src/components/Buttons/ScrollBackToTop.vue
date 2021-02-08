4a453af85dda2b94a00e2b1da67b5712f1342e41
daniilorain <dnlorn@gmail.com>
2021-02-08 17:06:38 +0300

    Show holders info while scrolling

---
+++ src/components/Buttons/ScrollBackToTop.vue
@@ -0,0 +1,34 @@
+<template>
+  <v-btn
+      v-scroll="onScroll"
+      v-show="fab"
+      fab
+      dark
+      fixed
+      bottom
+      right
+      color="primary"
+      @click="toTop"
+  >
+    <v-icon>mdi-arrow-up</v-icon>
+  </v-btn>
+</template>
+
+<script>
+export default {
+  name: "ScrollBackToTop",
+  methods: {
+    onScroll (e) {
+      if (typeof window === 'undefined') return
+      const top = window.pageYOffset || e.target.scrollTop || 0
+      this.fab = top > window.outerHeight * 2
+    },
+    toTop () {
+      this.$vuetify.goTo(0)
+    }
+  },
+  data: () => ({
+    fab: false
+  }),
+}
+</script>
--- src/views/contract/TokensTab/HoldersInfo.vue
+++ src/views/contract/TokensTab/HoldersInfo.vue
@@ -1,9 +1,9 @@
 <template>
-  <v-list class="pb-0">
+  <v-list class="pb-0" v-scroll="onScroll">
     <v-subheader class="title">Holders</v-subheader>
     <div class="bt-1 mt-1">
       <v-row
-          v-for="holder_address in Object.keys(holders)"
+          v-for="holder_address in holdersToShow"
           v-bind:key="holder_address"
           no-gutters
           class="py-1 bb-1 holders-row"
@@ -32,15 +32,37 @@
         </v-col>
       </v-row>
     </div>
+    <ScrollBackToTop />
   </v-list>
 </template>
 
 <script>
+import ScrollBackToTop from "@/components/Buttons/ScrollBackToTop";
 export default {
   name: "HoldersInfo",
+  components: {ScrollBackToTop},
   props: {
     holders: Object,
     token: String,
+  },
+  computed: {
+    holdersToShow() {
+      return Object.keys(this.holders).slice(0, this.shownAmount);
+    }
+  },
+  methods: {
+    onScroll() {
+      const isBottom = (window.innerHeight + window.scrollY) >= document.body.offsetHeight;
+      const isLeftHolders = Object.keys(this.holders).length > this.shownAmount;
+      if (isBottom && isLeftHolders) {
+        this.shownAmount += 10;
+      }
+    }
+  },
+  data() {
+    return {
+      shownAmount: 10,
+    }
   }
 }
 </script>
