75f4fbc456201f31ea70912c1f300f907e2b97d5
daniilorain <dnlorn@gmail.com>
2021-02-13 17:33:57 +0300

    add ImplementationsList

---
+++ src/views/contract/MetadataTab/ListBlocks/ImplementationsList.vue
@@ -0,0 +1,28 @@
+<template>
+  <v-list class="mt-3">
+    <v-subheader class="title">{{ name }}</v-subheader>
+    <EntrypointsCard
+        class="ml-3 mr-3"
+        :card-max-width="-1"
+        :entrypoints="implementsList"
+        :is-implementations="true"
+        :executable="executable"
+    />
+  </v-list>
+</template>
+
+<script>
+import EntrypointsCard from "@/components/Cards/EntrypointsCard";
+export default {
+  name: "ImplementationsList",
+  components: {EntrypointsCard},
+  props: {
+    name: String,
+    implementsList: Array,
+    executable: {
+      type: Boolean,
+      default: false,
+    }
+  }
+}
+</script>
