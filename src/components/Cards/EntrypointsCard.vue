470a487a5a1de202d5ae9482e5f377ce88882736
daniilorain <dnlorn@gmail.com>
2021-02-13 17:47:28 +0300

    add schema implementation components

--- src/components/Cards/EntrypointsCard.vue
+++ src/components/Cards/EntrypointsCard.vue
@@ -1,42 +1,66 @@
 <template>
-  <v-card flat outlined :style="`max-width:${cardMaxWidth}px;`">
-    <v-navigation-drawer floating permanent :style="`max-height: ${navigationMaxHeight}vh; width: 100%;`">
-      <v-expansion-panels
-          flat
-          accordion
-          mandatory
-          active-class="entrypoint-selected"
-          v-model="selected"
-      >
-        <v-expansion-panel
-            v-for="(item, i) in entrypoints"
-            :key="i"
-            :class="i > 0 ? 'bt-1' : ''"
-            class="entrypoint-panel"
-        >
-          <v-expansion-panel-header>
-            <div class="d-flex">
-              <span class="hash">{{ item.name }}</span>
-            </div>
-          </v-expansion-panel-header>
-          <v-expansion-panel-content>
-            <TypeDef :typedef="item.typedef" first="parameter"/>
-          </v-expansion-panel-content>
-        </v-expansion-panel>
-      </v-expansion-panels>
-    </v-navigation-drawer>
-  </v-card>
+  <v-row>
+    <v-col v-if="executable && selectedImplementation" cols="6">
+      <SchemaImplementation
+          :implementation="selectedImplementation"
+      />
+    </v-col>
+    <v-col :cols="executable ? 6 : 12">
+      <v-card flat outlined :style="`max-width:${cardMaxWidth >= 0 ? cardMaxWidth + 'px' : '100%'};`">
+        <v-navigation-drawer floating permanent :style="`max-height: ${navigationMaxHeight}vh; width: 100%;`">
+          <v-expansion-panels
+              flat
+              accordion
+              mandatory
+              active-class="entrypoint-selected"
+              v-model="selected"
+          >
+            <v-expansion-panel
+                v-for="(item, i) in entrypoints"
+                :key="i"
+                :class="i > 0 ? 'bt-1' : ''"
+                class="entrypoint-panel"
+            >
+              <v-expansion-panel-header>
+                <div class="d-flex">
+                  <span class="hash">{{ item.name }}</span>
+                </div>
+              </v-expansion-panel-header>
+              <v-expansion-panel-content v-if="isImplementations">
+                <ImplementationsList
+                    v-for="(implementations, i) in item.implementations"
+                    v-bind:key="`implementation${i}`"
+                    :implementations="implementations"
+                    @selectImplementation="setSelectedImplementation"
+                />
+              </v-expansion-panel-content>
+              <v-expansion-panel-content v-else>
+                <TypeDef :typedef="item.typedef" first="parameter"/>
+              </v-expansion-panel-content>
+            </v-expansion-panel>
+          </v-expansion-panels>
+        </v-navigation-drawer>
+      </v-card>
+    </v-col>
+  </v-row>
 </template>
 
 <script>
 import TypeDef from "@/views/contract/TypeDef";
+import ImplementationsList from "@/components/Lists/ImplementationsList";
+import SchemaImplementation from "@/views/contract/MetadataTab/SchemaImplementation/SchemaImplementation";
 
 export default {
   name: "EntrypointsCard",
-  components: {TypeDef},
+  components: {SchemaImplementation, ImplementationsList, TypeDef},
   props: {
     entrypoints: Array,
     selectedOutside: Number,
+    executable: Boolean,
+    isImplementations: {
+      type: Boolean,
+      default: false,
+    },
     cardMaxWidth: {
       type: Number,
       default: 500
@@ -56,6 +80,7 @@ export default {
       handler(newVal, oldVal) {
         if (newVal !== oldVal) {
           this.$emit('selected', newVal)
+          this.setDefaultImplementation()
         }
       },
     }
@@ -63,11 +88,28 @@ export default {
   created() {
     if (typeof this.selectedOutside === "number") {
       this.selected = this.selectedOutside
+      this.setDefaultImplementation()
+    }
+  },
+  methods: {
+    setDefaultImplementation() {
+      if (this.entrypoints[this.selected].implementations) {
+        const firstImplementationsList = this.entrypoints[this.selected].implementations[0];
+        const firstImplementationName = Object.keys(firstImplementationsList)[0];
+        this.selectedImplementation = {
+          implementationName: firstImplementationName,
+          info: firstImplementationsList[firstImplementationName]
+        }
+      }
+    },
+    setSelectedImplementation(selectedImplementation) {
+      this.selectedImplementation = selectedImplementation;
     }
   },
   data() {
     return {
       selected: null,
+      selectedImplementation: null,
     }
   }
 }
---
+++ src/views/contract/MetadataTab/SchemaImplementation/SchemaImplementation.vue
@@ -0,0 +1,67 @@
+<template>
+  <v-card flat outlined>
+    <v-card-text class="pa-0 pt-6 data">
+      <SchemaHeader
+          :title="implementation.implementationName"
+      />
+      <SchemaImplementationForm
+          :parameters="implementation.info"
+          :schema="parametersSchema"
+      />
+      <SchemaAlertData
+          v-if="alertData"
+          :alert-data="alertData"
+          @dismiss="showAlertData('')"
+      />
+      <SchemaAlertCustomSuccess
+          v-if="successText"
+          :success-text="successText"
+          @dismiss="showSuccessMessage('')"
+      />
+    </v-card-text>
+  </v-card>
+</template>
+
+<script>
+import SchemaHeader from "@/components/schema/schemaComponents/SchemaHeader";
+import SchemaAlertData from "@/components/schema/schemaAlert/SchemaAlertData";
+import SchemaAlertCustomSuccess from "@/components/schema/schemaAlert/SchemaAlertCustomSuccess";
+import SchemaImplementationForm from "@/views/contract/MetadataTab/SchemaImplementation/SchemaImplementationForm";
+export default {
+  name: "SchemaImplementation",
+  components: {SchemaImplementationForm, SchemaAlertCustomSuccess, SchemaAlertData, SchemaHeader},
+  props: {
+    title: String,
+    alertData: String,
+    successText: String,
+    implementation: Object,
+  },
+  computed: {
+    parametersSchema() {
+      return {
+        "type": "object",
+        "properties": this.getSchemaProperties(this.implementation.info)
+      }
+    }
+  },
+  methods: {
+    showAlertData(msg) {
+      this.alertData = msg;
+    },
+    showSuccessMessage(msg) {
+      this.successText = msg;
+    },
+    getSchemaProperties(implementation) {
+      let properties = {};
+      Object.keys(implementation).forEach(key => {
+        properties[key] = {
+          type: "string",
+          title: key,
+          "x-display": "custom-contract"
+        }
+      })
+      return properties;
+    }
+  }
+}
+</script>
