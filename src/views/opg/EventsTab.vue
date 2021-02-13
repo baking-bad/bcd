6621681647107457f9acd759142228dfc32b4b19
daniilorain <dnlorn@gmail.com>
2021-02-13 17:36:03 +0300

    add implementations list to events tab

--- src/views/opg/EventsTab.vue
+++ src/views/opg/EventsTab.vue
@@ -1,12 +1,19 @@
 <template>
   <div class="fill-canvas canvas">
     <v-container fluid class="pa-8">
+      <ImplementationsList
+          name="Events"
+          :implements-list="[]"
+          executable
+      />
     </v-container>
   </div>
 </template>
 
 <script>
+import ImplementationsList from "@/views/contract/MetadataTab/ListBlocks/ImplementationsList";
 export default {
-  name: "EventsTab"
+  name: "EventsTab",
+  components: {ImplementationsList}
 }
 </script>
