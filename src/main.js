baking-bad/bcd

commit c0ffc92df7b1aa65ab0c4a877d180896e3e6772f

Remove auth (#282)
* Remove auth

* diff is back

* remove isAuthorized from diff

src/main.js
@@ -12,8 +12,7 @@ import * as Sentry from "@sentry/browser";
 import { Vue as VueIntegration } from "@sentry/integrations";
 
 import { shortcut, formatDatetime, formatDate, plural, urlExtractBase58, checkAddress, round } from "@/utils/tz.js";
-import { getJwt, logout, getBool } from "@/utils/auth.js";
-import { BetterCallApi, UnauthorizedError } from "@/api/bcd.js";
+import { BetterCallApi } from "@/api/bcd.js";
 import { NodeRPC } from "@/api/rpc.js";
 
 import { makeVuetify } from '@/plugins/vuetify';
@@ -89,10 +88,6 @@ let api = new BetterCallApi(config.API_URI);
 api.getConfig().then(response => {
   Object.assign(config, response);
 
-  if (config.sandbox_mode) {
-    config.HOME_PAGE = 'dashboard';
-  }
-
   let rpc = new NodeRPC(config.rpc_endpoints);
 
   let helpers = { shortcut, formatDatetime, formatDate, plural, checkAddress, round }
@@ -136,41 +131,6 @@ api.getConfig().then(response => {
     }
   ]);
 
-  router.beforeEach((to, from, next) => {
-    const privatePages = ['/dashboard', '/dashboard/'];
-    const authRequired = privatePages.includes(to.path);
-    const loggedIn = config.sandbox_mode || getJwt() !== null;
-
-    store.dispatch('setIsAuthorized', loggedIn)
-    if (authRequired && !loggedIn) {
-      return next('/');
-    }
-
-    if (loggedIn && store.state.profile === null) {
-      api.getProfile()
-        .then(res => {
-          let subs = res.subscriptions;
-          delete res['subscriptions'];
-          store.dispatch('setProfile', res);
-          store.dispatch('setSubscriptions', subs);
-        })
-        .catch(err => {
-          if (err instanceof UnauthorizedError) {
-            store.dispatch('setIsAuthorized', false);
-            store.dispatch('setProfile', null);
-            store.dispatch('showError', 'Unauthorized access. Please sign in');
-          } else {
-            store.dispatch('showError', err);
-          }
-          logout();
-          // eslint-disable-next-line
-          console.log(err)
-        });
-    }
-
-    next();
-  })
-
   if (config.GA_ENABLED || config.ga_enabled) {
     Vue.use(VueAnalytics, {
       id: "UA-160856677-1",
@@ -191,7 +151,7 @@ api.getConfig().then(response => {
     });
   }
 
-  const isDark = getBool('dark', true);
+  const isDark = localStorage.getItem('dark') ? localStorage.getItem : true;
   if (isDark) {
     document.body.classList.add('dark-theme-background');
   }

public/index.html
@@ -3,13 +3,13 @@
   <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=1280">
-    <meta name="description" content="Tezos smart contract explorer & developer dashboard, simplifies perception and facilitates interaction. By Baking Bad.">
+    <meta name="description" content="Tezos smart contract explorer simplifies perception and facilitates interaction. By Baking Bad.">
     <meta name="keywords" content="Tezos, blockchain, explorer, smart contract, block explorer, dapp, crypto, cryptocurrency, XTZ, search, analysis, michelson, ligo, smartpy, lorentz, token, big map, sandbox">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta property="og:url" content="<%= BASE_URL %>">
     <meta property="og:type" content="website">
     <meta property="og:title" content="Better Call Dev">
-    <meta property="og:description" content="Tezos smart contract explorer, developer dashboard, and API provider. Easy to spin up / integrate with your sandbox.">
+    <meta property="og:description" content="Tezos smart contract explorer and API provider. Easy to spin up / integrate with your sandbox.">
     <meta property="og:image" content="<%= BASE_URL %>img/logo_og.png">
     <meta property="og:image:secure_url" content="<%= BASE_URL %>img/logo_og.png">
     <meta property="og:image:type" content="image/png">
@@ -18,7 +18,7 @@
     <meta name="twitter:card" content="summary">
     <meta name="twitter:creator" content="@TezosBakingBad">
     <meta name="twitter:title" content="Better Call Dev">
-    <meta name="twitter:description" content="Tezos smart contract explorer, developer dashboard, and API provider. Easy to spin up / integrate with your sandbox.">
+    <meta name="twitter:description" content="Tezos smart contract explorer and API provider. Easy to spin up / integrate with your sandbox.">
     <meta name="twitter:image" content="<%= BASE_URL %>img/logo_og.png">
     <link rel="icon" href="<%= BASE_URL %>img/favicon.ico">
     <title>Better Call Dev — Tezos smart contract explorer by Baking Bad</title>

src/api/bcd.js
@@ -1,7 +1,6 @@
 const axios = require('axios').default;
 const maxSize = 10;
 
-import { getJwt } from '@/utils/auth.js'
 import { getCancellable, postCancellable, cancelRequests } from '@/utils/cancellation.js';
 
 export class RequestFailedError extends Error {
@@ -11,13 +10,6 @@ export class RequestFailedError extends Error {
     if (typeof response !== 'string') {
         this.message = response.statusText;
     }
-  }
-}
-
-export class UnauthorizedError extends Error {
-  constructor(message) {
-    super(message);
-    this.name = "UnauthorizedError";
   }
 }
 
@@ -89,11 +81,6 @@ export class BetterCallApi {
 
   getContract(network, address) {
     let params = {};
-    const token = getJwt();
-    if (token) {
-      params.headers = { 'Authorization': token }
-    }
-
     return getCancellable(this.api, `/contract/${network}/${address}`, params)
       .then((res) => {
         if (!res) { return res; }
@@ -567,179 +554,6 @@ export class BetterCallApi {
       })
   }
 
-  vote(sn, sa, dn, da, vote) {
-    return this.api.post(`/profile/vote`, {
-      src: sa,
-      src_network: sn,
-      dest: da,
-      dest_network: dn,
-      vote: vote,
-    },
-      {
-        headers: {
-          'Authorization': getJwt()
-        }
-      })
-      .then((res) => {
-        return res.data
-      })
-      .catch((err) => {
-        if (err.response !== undefined && err.response.status == 401) {
-          throw new UnauthorizedError(err);
-        }
-        throw err;
-      })
-  }
-
-  getTasks() {
-    return this.api.get(`/profile/vote/tasks`,
-      {
-        headers: {
-          'Authorization': getJwt()
-        }
-      })
-      .then((res) => {
-        return res.data
-      })
-      .catch((err) => {
-        if (err.response !== undefined && err.response.status == 401) {
-          throw new UnauthorizedError(err);
-        }
-        throw err;
-      })
-  }
-
-  generateTasks() {
-    return this.api.get(`/profile/vote/generate`,
-      {
-        headers: {
-          'Authorization': getJwt()
-        }
-      })
-      .then((res) => {
-        return res.data
-      })
-      .catch((err) => {
-        if (err.response !== undefined && err.response.status == 401) {
-          throw new UnauthorizedError(err);
-        }
-        throw err;
-      })
-  }
-
-  getProfile() {
-    return this.api.get(`/profile`,
-      {
-        headers: {
-          'Authorization': getJwt()
-        }
-      })
-      .then((res) => {
-        return res.data
-      })
-      .catch((err) => {
-        if (err.response !== undefined && err.response.status == 401) {
-          throw new UnauthorizedError(err);
-        }
-        throw err;
-      })
-  }
-
-  profileMarkAllRead(timestamp) {
-    return this.api.post(`/profile/mark_all_read`, { timestamp },
-      {
-        headers: {
-          'Authorization': getJwt()
-        }
-      })
-      .then((res) => {
-        return res.data
-      })
-      .catch((err) => {
-        if (err.response !== undefined && err.response.status == 401) {
-          throw new UnauthorizedError(err);
-        }
-        throw err;
-      })
-  }
-
-  getProfileSubscriptions() {
-    return this.api.get(`/profile/subscriptions`, {
-      headers: {
-        'Authorization': getJwt()
-      }
-    })
-      .then((res) => {
-        return res.data
-      })
-      .catch((err) => {
-        if (err.response !== undefined && err.response.status == 401) {
-          throw new UnauthorizedError(err);
-        }
-        throw err;
-      })
-  }
-
-  addProfileSubscription(subscription) {
-    return this.api.post(`/profile/subscriptions`, subscription,
-      {
-        headers: {
-          'Authorization': getJwt()
-        }
-      })
-      .then((res) => {
-        return res.data
-      })
-      .catch((err) => {
-        if (err.response !== undefined && err.response.status == 401) {
-          throw new UnauthorizedError(err);
-        }
-        throw err;
-      })
-  }
-
-  removeProfileSubscription(network, address) {
-    return this.api.delete(`/profile/subscriptions`, {
-      headers: {
-        'Authorization': getJwt()
-      },
-      data: {
-        network, address
-      }
-    })
-      .then((res) => {
-        return res.data
-      })
-      .catch((err) => {
-        if (err.response !== undefined && err.response.status == 401) {
-          throw new UnauthorizedError(err);
-        }
-        throw err;
-      })
-  }
-
-  getProfileEvents(offset = 0) {
-    return this.api.get(`/profile/subscriptions/events`,
-      {
-        headers: {
-          'Authorization': getJwt()
-        },
-        params: {
-          offset: offset,
-          size: maxSize
-        }
-      })
-      .then((res) => {
-        return res.data
-      })
-      .catch((err) => {
-        if (err.response !== undefined && err.response.status == 401) {
-          throw new UnauthorizedError(err);
-        }
-        throw err;
-      })
-  }
-
   getStats() {
     return getCancellable(this.api, `/stats`, {})
       .then((res) => {
@@ -871,161 +685,6 @@ export class BetterCallApi {
     return getCancellable(this.api, `/tokens/${network}/series?${params.join('&')}`, {})
       .then((res) => {
         if (!res) { return res; }
-        if (res.status != 200) {
-          throw new RequestFailedError(res);
-        }
-        return res.data
-      })
-  }
-
-  getProfileAccounts() {
-    return getCancellable(this.api, `/profile/accounts`, {
-      headers: {
-        'Authorization': getJwt()
-      }
-    })
-      .then((res) => {
-        if (res.status != 200) {
-          throw new RequestFailedError(res);
-        }
-        return res.data
-      })
-  }
-
-  getProfileRepos(login) {
-    return getCancellable(this.api, `/profile/repos?login=${login}`, {
-      headers: {
-        'Authorization': getJwt()
-      }
-    })
-      .then((res) => {
-        if (res.status != 200) {
-          throw new RequestFailedError(res);
-        }
-        return res.data
-      })
-  }
-
-  getProfileRefs(owner, repo) {
-    return getCancellable(this.api, `/profile/refs?owner=${owner}&repo=${repo}`, {
-      headers: {
-        'Authorization': getJwt()
-      }
-    })
-      .then((res) => {
-        if (res.status != 200) {
-          throw new RequestFailedError(res);
-        }
-        return res.data
-      })
-  }
-
-  getProfileCompilations(limit = 0, offset = 0) {
-    let params = [];
-    if (limit > 0) {
-      params.push(`limit=${limit}`)
-    }
-    if (offset > 0) {
-      params.push(`offset=${offset}`)
-    }
-    return getCancellable(this.api, `/profile/compilations?${params.join('&')}`, {
-      headers: {
-        'Authorization': getJwt()
-      }
-    })
-      .then((res) => {
-        if (res.status != 200) {
-          throw new RequestFailedError(res);
-        }
-        return res.data
-      })
-  }
-
-  getVerificationList() {
-    return getCancellable(this.api, `/profile/compilations/verification`, {
-      headers: {
-        'Authorization': getJwt()
-      },
-    })
-      .then((res) => {
-        if (res.status != 200) {
-          throw new RequestFailedError(res);
-        }
-        return res.data
-      })
-  }
-
-  verifyContract(network, address, account, repo, ref) {
-    return this.api.post(`/profile/compilations/verification`, {
-      network: network,
-      address: address,
-      account: account,
-      repo: repo,
-      ref: ref,
-    }, {
-      headers: {
-        'Authorization': getJwt()
-      },
-    })
-      .then((res) => {
-        if (res.status != 200) {
-          throw new RequestFailedError(res);
-        }
-        return res.data
-      })
-  }
-
-  getDeploymentList(limit, offset) {
-    let params = [];
-    if (limit > 0) {
-      params.push(`limit=${limit}`)
-    }
-    if (offset > 0) {
-      params.push(`offset=${offset}`)
-    }
-    return getCancellable(this.api, `/profile/compilations/deployment?${params.join('&')}`, {
-      headers: {
-        'Authorization': getJwt()
-      },
-    })
-      .then((res) => {
-        if (res.status != 200) {
-          throw new RequestFailedError(res);
-        }
-        return res.data
-      })
-  }
-
-  deployContract(network, address, repo, ref) {
-    return this.api.post(`/profile/compilations/deployment`, {
-      network: network,
-      address: address,
-      repo: repo,
-      ref: ref,
-    }, {
-      headers: {
-        'Authorization': getJwt()
-      },
-    })
-      .then((res) => {
-        if (res.status != 200) {
-          throw new RequestFailedError(res);
-        }
-        return res.data
-      })
-  }
-
-  finalizeDeployContract(hash, taskId, resultId) {
-    return this.api.patch(`/profile/compilations/deployment`, {
-      operation_hash: hash,
-      task_id: taskId,
-      result_id: resultId,
-    }, {
-      headers: {
-        'Authorization': getJwt()
-      },
-    })
-      .then((res) => {
         if (res.status != 200) {
           throw new RequestFailedError(res);
         }

src/components/Dialogs/VerifyDialog.vue
@@ -1,271 +1,0 @@
-<template>
-  <v-dialog v-model="value" persistent max-width="800">
-    <v-card>
-      <v-card-title class="d-flex flex-column align-start">
-        <span class="title-1">Contract verification</span>
-        <div v-if="address">
-          <span class="hash text--secondary">{{ address }}</span
-          >&nbsp;
-          <span class="overline" v-if="network">{{ network }}</span>
-        </div>
-      </v-card-title>
-      <v-card-text>
-        <v-stepper v-model="step" vertical class="elevation-0">
-          <v-stepper-step :complete="selectedAccount >= 0" step="1" editable>
-            {{
-              selectedAccount === -1
-                ? "Select account"
-                : accounts[selectedAccount].login
-            }}
-            <small>{{
-              selectedAccount === -1
-                ? "Only public accounts are availiable"
-                : "Selected account"
-            }}</small>
-          </v-stepper-step>
-          <v-stepper-content step="1">
-            <v-skeleton-loader :loading="loadingAccounts" type="list-item@3">
-              <v-list
-                dense
-                v-if="accounts && accounts.length"
-                class="py-0 overflow-y-auto"
-                max-height="400"
-              >
-                <v-list-item-group v-model="selectedAccount">
-                  <template v-for="(acc, i) in accounts">
-                    <v-list-item :key="i" @click="onAccountSelected(acc.login)">
-                      <v-list-item-avatar>
-                        <v-img :src="acc.avatarURL" />
-                      </v-list-item-avatar>
-                      <v-list-item-content>
-                        <v-list-item-title>{{ acc.login }}</v-list-item-title>
-                      </v-list-item-content>
-                    </v-list-item>
-                    <v-divider
-                      :key="`divider-${i}`"
-                      v-if="i < accounts.length - 1"
-                    />
-                  </template>
-                </v-list-item-group>
-              </v-list>
-              <div v-else>There aren't any accounts</div>
-            </v-skeleton-loader>
-          </v-stepper-content>
-
-          <v-stepper-step :complete="selectedRepo >= 0" step="2" editable>
-            {{
-              selectedRepo === -1
-                ? "Select repository"
-                : repos[selectedRepo].project
-            }}
-            <small>{{
-              selectedRepo === -1
-                ? "Only public repos are availiable"
-                : "Selected repository"
-            }}</small>
-          </v-stepper-step>
-          <v-stepper-content step="2">
-            <v-skeleton-loader :loading="loadingRepos" type="list-item@3">
-              <v-list
-                dense
-                v-if="repos && repos.length"
-                class="py-0 overflow-y-auto"
-                max-height="400"
-              >
-                <v-list-item-group v-model="selectedRepo">
-                  <template v-for="(repo, i) in repos">
-                    <v-list-item :key="i" @click="onRepoSelected(repo)">
-                      <v-list-item-content>
-                        <v-list-item-title>{{
-                          repo.project
-                        }}</v-list-item-title>
-                        <v-list-item-subtitle>{{
-                          repo.url
-                        }}</v-list-item-subtitle>
-                      </v-list-item-content>
-                    </v-list-item>
-                    <v-divider
-                      :key="`divider-${i}`"
-                      v-if="i < repos.length - 1"
-                    />
-                  </template>
-                </v-list-item-group>
-              </v-list>
-              <div v-else>There aren't any repositories</div>
-            </v-skeleton-loader>
-          </v-stepper-content>
-
-          <v-stepper-step :complete="selectedTag >= 0" step="3" editable>
-            {{
-              selectedTag === -1 ? "Select branch/tag" : refs[selectedTag].name
-            }}
-            <small>{{
-              selectedTag === -1
-                ? "Contract code in branch must be equal code in chain"
-                : "Selected branch/tag"
-            }}</small>
-          </v-stepper-step>
-          <v-stepper-content step="3">
-            <v-skeleton-loader :loading="loadingRefs" type="list-item@3">
-              <v-list
-                dense
-                v-if="refs && refs.length"
-                class="py-0 overflow-y-auto"
-                max-height="400"
-              >
-                <v-list-item-group v-model="selectedTag">
-                  <template v-for="(tag, i) in refs">
-                    <v-list-item :key="i" @click="step = 3">
-                      <v-list-item-content>
-                        <v-list-item-title>{{ tag.name }}</v-list-item-title>
-                        <v-list-item-subtitle>{{
-                          tag.type
-                        }}</v-list-item-subtitle>
-                      </v-list-item-content>
-                    </v-list-item>
-                    <v-divider
-                      :key="`divider-${i}`"
-                      v-if="i < tag.length - 1"
-                    />
-                  </template>
-                </v-list-item-group>
-              </v-list>
-              <div v-else>No refs in this repository</div>
-            </v-skeleton-loader>
-          </v-stepper-content>
-        </v-stepper>
-      </v-card-text>
-      <v-card-actions>
-        <v-spacer></v-spacer>
-        <v-btn
-          @click="verify"
-          :disabled="selectedRepo == -1 || selectedTag == -1"
-          :loading="verification"
-          >Verify</v-btn
-        >
-        <v-btn text @click="close" :disabled="verification">Cancel</v-btn>
-      </v-card-actions>
-    </v-card>
-  </v-dialog>
-</template>
-
-<script>
-import { mapActions } from "vuex";
-
-export default {
-  name: "VerifyDialog",
-  props: {
-    value: Boolean,
-    address: String,
-    network: String,
-  },
-  data: () => ({
-    step: 1,
-    loadingAccounts: false,
-    loadingRepos: false,
-    loadingRefs: false,
-    accounts: [],
-    repos: [],
-    refs: [],
-    selectedAccount: -1,
-    selectedRepo: -1,
-    selectedTag: -1,
-    verification: false,
-  }),
-  methods: {
-    ...mapActions(["showError", "showSuccess"]),
-    close() {
-      this.$emit("input", false);
-    },
-    getAccounts() {
-      this.loadingAccounts = true;
-      this.api
-        .getProfileAccounts()
-        .then((res) => {
-          this.accounts = res;
-        })
-        .catch((err) => {
-          console.log(err);
-          this.showError(err);
-        })
-        .finally(() => {
-          this.loadingAccounts = false;
-        });
-    },
-    getRepos(login) {
-      this.loadingRepos = true;
-      this.api
-        .getProfileRepos(login)
-        .then((res) => {
-          this.repos = res;
-        })
-        .catch((err) => {
-          console.log(err);
-          this.showError(err);
-        })
-        .finally(() => {
-          this.loadingRepos = false;
-        });
-    },
-    getRefs(owner, repo) {
-      this.loadingRefs = true;
-      this.api
-        .getProfileRefs(owner, repo)
-        .then((res) => {
-          this.refs = res;
-        })
-        .catch((err) => {
-          console.log(err);
-          this.showError(err);
-        })
-        .finally(() => {
-          this.loadingRefs = false;
-        });
-    },
-    verify() {
-      this.verification = true;
-      this.api
-        .verifyContract(
-          this.network,
-          this.address,
-          this.accounts[this.selectedAccount].login,
-          this.repos[this.selectedRepo].project,
-          this.refs[this.selectedTag].name
-        )
-        .then(() => {
-          this.showSuccess("Contract verification is in processing");
-        })
-        .catch((err) => {
-          console.log(err);
-          this.showError(err);
-        })
-        .finally(() => {
-          this.verification = false;
-          this.selectedAccount = -1;
-          this.selectedRepo = -1;
-          this.selectedTag = -1;
-          this.step = 1;
-          this.close();
-        });
-    },
-    onAccountSelected(item) {
-      this.selectedRepo = -1;
-      this.selectedTag = -1;
-      this.step = 2;
-      this.getRepos(item);
-    },
-    onRepoSelected(item) {
-      this.selectedTag = -1;
-      this.step = 3;
-      this.getRefs(item.user, item.project);
-    },
-  },
-  watch: {
-    value: function (newValue) {
-      if (!newValue) return;
-      if (!this.accounts || this.accounts.length > 0) return;
-      this.getAccounts();
-    },
-  },
-};
-</script>

commit 881ffc49dd3a12542b7665f16497a07a1e27fb91

Fix `isDark` check to from localStorage value
Looks like this was a regression introduced in https://github.com/baking-bad/bcd/commit/c0ffc92df7b1aa65ab0c4a877d180896e3e6772f#diff-27653c212e1cfe533e4eb2f7d0d3f89604c9de48a09583b4cbbbcbd08a07da79R154, things will always evaluate to `true`. 

This PR fixes it by parsing whatever the value is as JSON so if it's `"true"` or `"false"` it should work properly, and then default to dark mode 👍!

src/main.js
@@ -155,7 +155,7 @@ api.getConfig().then(response => {
     });
   }
 
-  const isDark = localStorage.getItem('dark') ? localStorage.getItem : true;
+  const isDark = localStorage.getItem('dark') ? JSON.parse(localStorage.getItem('dark')) : true;
   if (isDark) {
     document.body.classList.add('dark-theme-background');
   }