53a2c3c73922fde0e640e7f8349c485d75ee6f37
Daniil Orain <dnlorn@gmail.com>
2021-08-20 12:53:30 +0300

    Use native sharing api

---
+++ src/components/Buttons/ShareLink.vue
@@ -0,0 +1,47 @@
+<template>
+  <v-tooltip
+    :top="top"
+    :bottom="bottom"
+  >
+    <template v-slot:activator="{ on }">
+      <v-btn
+        v-on="on"
+        icon
+        @click="openNativeSharing"
+      >
+        <v-icon class="text--secondary">mdi-share-variant</v-icon>
+      </v-btn>
+    </template>
+    Share link
+  </v-tooltip>
+</template>
+
+<script>
+import {mapActions} from "vuex";
+
+export default {
+  name: "ShareLink",
+  props: {
+    top: Boolean,
+    bottom: Boolean,
+    alias: String,
+    link: String,
+  },
+  methods: {
+    ...mapActions(["showError", "showClipboardOK"]),
+    openNativeSharing() {
+      if (navigator.share) {
+        navigator.share({
+          title: this.alias,
+          url: this.link
+        }).then(() => {
+          this.showClipboardOK();
+        }).catch(this.showError);
+      } else {
+        this.$clipboard(this.link);
+        this.showClipboardOK();
+      }
+    }
+  }
+}
+</script>
