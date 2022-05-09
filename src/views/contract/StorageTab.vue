e05680a7a9b85f3238a96fe4af01c41cf495c9e9
Daniil Orain <dnlorn@gmail.com>
2022-05-09 20:13:08 +0400

    update storage tab

--- src/views/contract/StorageTab.vue
+++ src/views/contract/StorageTab.vue
@@ -10,106 +10,116 @@
       </v-col>
     </v-row>
     <v-skeleton-loader :loading="loading" type="card-heading, image">
-      <v-card v-if="storage || rawStorage" tile flat outlined class="pa-0">
-        <v-card-title class="d-flex sidebar px-4 py-3">
-          <v-select
-            v-if="storageVersions.length > 0"
-            v-model="level"
-            @change="getStorage(true)"
-            :items="storageVersions"
-            item-text="version"
-            item-value="level"
-            style="max-width: 175px"
-            rounded
-            dense
-            background-color="data"
-            class="mb-1"
-            hide-details
-          ></v-select>
-          <v-spacer></v-spacer>
-          <v-btn @click="showRaw = true" small text class="text--secondary">
-            <v-icon class="mr-1" small>mdi-code-json</v-icon>
-            <span>Raw JSON</span>
-          </v-btn>
-          <v-tooltip v-if="!raw" top>
-            <template v-slot:activator="{ on }">
-              <v-btn
-                v-on="on"
-                small
-                text
-                class="ml-2 text--secondary"
-                @click="downloadFile"
-                :loading="downloading"
-                :disabled="downloading"
-              >
-                <v-icon class="mr-1" small>mdi-download-outline</v-icon>
-                <span>Full dump</span>
+      <v-row>
+        <v-col cols="8">
+          <h3 v-if="storage || rawStorage" class="mb-3 text--secondary outline font-weight-medium">Storage</h3>
+          <v-card v-if="storage || rawStorage" tile flat outlined class="pa-0">
+            <v-card-title class="d-flex sidebar px-4 py-3">
+              <v-select
+                v-if="storageVersions.length > 0"
+                v-model="level"
+                @change="getStorage(true)"
+                :items="storageVersions"
+                item-text="version"
+                item-value="level"
+                style="max-width: 175px"
+                rounded
+                dense
+                background-color="data"
+                class="mb-1"
+                hide-details
+              ></v-select>
+              <v-spacer></v-spacer>
+              <v-btn @click="showRaw = true" small text class="text--secondary">
+                <v-icon class="mr-1" small>mdi-code-json</v-icon>
+                <span>Raw JSON</span>
               </v-btn>
-            </template>
-            Raw snapshot with all big map data
-          </v-tooltip>
-          <v-btn
-            v-if="rawStorage && raw"
-            @click="
+              <v-tooltip v-if="!raw" top>
+                <template v-slot:activator="{ on }">
+                  <v-btn
+                    v-on="on"
+                    small
+                    text
+                    class="ml-2 text--secondary"
+                    @click="downloadFile"
+                    :loading="downloading"
+                    :disabled="downloading"
+                  >
+                    <v-icon class="mr-1" small>mdi-download-outline</v-icon>
+                    <span>Full dump</span>
+                  </v-btn>
+                </template>
+                Raw snapshot with all big map data
+              </v-tooltip>
+              <v-btn
+                v-if="rawStorage && raw"
+                @click="
               () => {
                 $clipboard(getStorageString());
                 showClipboardOK();
               }
             "
-            class="ml-2"
-            small
-            text
-          >
-            <v-icon class="mr-1" small>mdi-content-copy</v-icon>
-            <span class="text--secondary">Copy</span>
-          </v-btn>
-          <v-btn
-            v-if="raw"
-            @click="getStorage()"
-            class="ml-2 text--secondary"
-            small
-            text
-          >
-            <v-icon class="mr-1" small>mdi-file-tree</v-icon>
-            <span>Switch to Tree View</span>
-          </v-btn>
-          <v-btn
-            v-else
-            @click="getStorageRaw()"
-            class="ml-2 text--secondary"
-            small
-            text
-          >
-            <v-icon class="mr-1" small>mdi-code-parentheses</v-icon>
-            <span>Switch to Micheline</span>
-          </v-btn>
-        </v-card-title>
-        <v-card-text class="pa-0 data">
-          <v-row no-gutters>
-            <v-col cols="8">
-              <Michelson v-if="raw" :code="rawStorage"></Michelson>
-              <div v-else class="py-4 data">
-                <MiguelTreeView
-                    :miguel="storage"
-                    :network="network"
-                    openAll
-                    compact-pair
-                />
-              </div>
-            </v-col>
-            <v-divider vertical></v-divider>
-            <v-col v-if="schema">
+                class="ml-2"
+                small
+                text
+              >
+                <v-icon class="mr-1" small>mdi-content-copy</v-icon>
+                <span class="text--secondary">Copy</span>
+              </v-btn>
+              <v-btn
+                v-if="raw"
+                @click="getStorage()"
+                class="ml-2 text--secondary"
+                small
+                text
+              >
+                <v-icon class="mr-1" small>mdi-file-tree</v-icon>
+                <span>Switch to Tree View</span>
+              </v-btn>
+              <v-btn
+                v-else
+                @click="getStorageRaw()"
+                class="ml-2 text--secondary"
+                small
+                text
+              >
+                <v-icon class="mr-1" small>mdi-code-parentheses</v-icon>
+                <span>Switch to Micheline</span>
+              </v-btn>
+            </v-card-title>
+            <v-card-text class="pa-0 data">
+              <v-row no-gutters>
+                <v-col cols="8">
+                  <Michelson v-if="raw" :code="rawStorage"></Michelson>
+                  <div v-else class="py-4 data">
+                    <MiguelTreeView
+                      :miguel="storage"
+                      :network="network"
+                      openAll
+                      compact-pair
+                    />
+                  </div>
+                </v-col>
+                <v-divider vertical></v-divider>
+              </v-row>
+            </v-card-text>
+          </v-card>
+          <ErrorState v-else />
+        </v-col>
+        <v-col cols="4">
+          <h3 v-if="schema" class="mb-3 text--secondary outline font-weight-medium">Type</h3>
+          <v-card v-if="schema" tile flat outlined class="pa-0">
+            <v-card-text>
               <TypeDef
                 :typedef="schema.typedef"
                 first="storage"
                 class="pt-3 pb-1 px-6"
                 style="opacity: 0.8"
               />
-            </v-col>
-          </v-row>
-        </v-card-text>
-      </v-card>
-      <ErrorState v-else />
+            </v-card-text>
+          </v-card>
+        </v-col>
+      </v-row>
     </v-skeleton-loader>
     <RawJsonViewer
       :show.sync="showRaw"
