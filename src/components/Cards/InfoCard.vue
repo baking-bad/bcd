f1546ed2a10ca197bec312b0a2840ad0eaa81102
Artem <aopoltorzhicky@gmail.com>
2022-06-08 15:17:08 +0300

    Fix: icon margin

--- src/components/Cards/InfoCard.vue
+++ src/components/Cards/InfoCard.vue
@@ -1,6 +1,6 @@
 <template>
   <v-card flat class="mt-10 pa-10 d-flex flex-column align-center justify-center transparent">
-    <v-img src="@/assets/relocation.png" max-height="100"></v-img>    
+    <v-img src="@/assets/relocation.png" max-height="100" class="icon"></v-img>    
     <div class="information font-weight-regular mt-2"><slot /></div>
   </v-card>
 </template>
@@ -23,5 +23,9 @@ export default {
   line-height: 160%;
   text-align: center;
   color: #959595;
+}
+
+.icon {
+  margin-right: 30px
 }
 </style>
