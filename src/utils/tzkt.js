86cd8db0dda3826b1c239802566e746819379583
Artem <aopoltorzhicky@gmail.com>
2022-06-09 19:17:16 +0300

    OPG page

--- src/utils/tzkt.js
+++ src/utils/tzkt.js
@@ -7,6 +7,12 @@ export function openTzktContract(network, contract) {
   window.open(`https://${network === 'mainnet' ? '' : `${network}.`}tzkt.io/${contract.address}`, '_blank');
 }
 
+
+export function openTzktOPG(network, hash) {
+  window.open(`https://${network === 'mainnet' ? '' : `${network}.`}tzkt.io/${hash}`, '_blank');
+}
+
+
 export default class TZKTBlockExplorer extends BlockExplorer {
   constructor() {
     const rpcUrls = {
