b6148da22294f2334059bc6f48045ce805113911
Artem <aopoltorzhicky@gmail.com>
2022-06-21 13:28:37 +0300

    Fix: code smells

--- src/components/schema/schemaDialog/SchemaCmdLine.vue
+++ src/components/schema/schemaDialog/SchemaCmdLine.vue
@@ -82,8 +82,7 @@ export default {
   },
   methods: {
     isNumber(evt) {
-      evt = (evt) ? evt : window.event;
-      var charCode = (evt.which) ? evt.which : evt.keyCode;
+      const charCode = (evt.which) ? evt.which : evt.keyCode;
       if ((charCode > 31 && (charCode < 48 || charCode > 57)) && charCode !== 46) {
         evt.preventDefault();
       } else {
