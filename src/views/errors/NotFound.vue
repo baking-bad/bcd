ad87747f5614dc2469f63bb0aa62191d705456c6
Artem <aopoltorzhicky@gmail.com>
2022-08-09 15:53:17 +0300

    Fix: discord link in 404

--- src/views/errors/NotFound.vue
+++ src/views/errors/NotFound.vue
@@ -6,7 +6,7 @@
       <template #description>
         <span class='body-1 font-weight-light text--secondary mt-1'>
           Please, try again or write to our team in&nbsp;
-          <a class='no-decoration' href='https://discord.gg/aG8XKuwsQd' target='_blank' rel="noopener">Discord</a>.
+          <a class='no-decoration' href='https://discord.gg/4QgKst2hF7' target='_blank' rel="noopener">Discord</a>.
         </span>
       </template>
     </EmptyState>
