7b3336ded426ce792637d60d6932c3cca47f4a9b
Artem <aopoltorzhicky@gmail.com>
2022-08-15 12:34:11 +0300

    Fix: build error

--- src/components/schema/Schema.vue
+++ src/components/schema/Schema.vue
@@ -318,7 +318,7 @@ export default {
 
       return async () => {
         this.fireEvent("Tezos Client", "fork");
-        await this.fork(false);
+        await this.fork();
       }
     },
     setExecuteActions() {
@@ -523,10 +523,8 @@ export default {
         })
         .finally(() => (this.execution = false));
     },
-    async fork(isLast) {
-      if (this.execution) return;
-
-      
+    async fork() {
+      if (this.execution) return;      
 
       this.execution = true;
       try {
