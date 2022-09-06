9c4c435eea7da0808661e5eee7e8e3a7544b284f
Artem Poltorzhitskiy <aopoltorzhicky@gmail.com>
2022-09-06 16:06:43 +0300

    Fix: network validation in URL (#465)

--- src/router/index.js
+++ src/router/index.js
@@ -40,274 +40,252 @@ import ContractStatsTab from "@/views/contract/StatsTab.vue";
 
 Vue.use(VueRouter)
 
-const router = new Router({
-  linkActiveClass: '',
-  linkExactActiveClass: '',
-  mode: 'history',
-  scrollBehavior () {
-    window.scrollTo(0,0)
-  },
-  routes: [
-    {
-      path: '/',
-      components: {
-        default: Home
-      },
-      name: 'home'
-    },
-    {
-      path: '/not_found',
-      components: {
-        default: NotFound,
-      },
-      name: 'not_found',
-    },
-    {
-      path: '/search',
-      components: {
-        default: ExtendedSearch
-      },
-      name: 'search',
-      props: { default: true },
-    },
-    {
-      path: '/dapps*',
-      props: { default: true },
-      redirect() {
-        window.location.href = 'https://tzkt.io/dapps'
-      }
-    },
-    {
-      path: '/deploy',
-      components: {
-        default: Deploy,
-      },
-      name: 'deploy',
-      props: { default: true },
-    },
-    {
-      path: '/docs',
-      components: {
-        default: Docs
-      },
-      name: 'docs',
-      props: { default: true },
-    },
-    {
-      path: '/constant/:network/:address',
-      components: {
-        default: Constant,
-      },
-      props: { default: true },
-      children: [
-        {
-          path: '',
-          name: 'constant',
-          component: ConstantTab,
-          props: true,
-        },
-      ]
-    },
-    {
-      path: '/:network',
-      components: {
-        default: Network,
-      },
-      props: { default: true },
-      children: [
-        {
-          path: '',
-          name: 'network',
-          redirect: 'recently_called'
-        },{
-          path: 'recently_called',
-          name: 'recently_called',
-          component: RecentlyCalledTab,
-          props: true,
-        },{
-          path: 'constants',
-          name: 'network_constants',
-          component: ConstantsTab,
-          props: true,
-        },{
-          path: 'stats',
-          name: 'network_stats',
-          component: StatsTab,
-          props: true,
-        },
-      ]
-    },
-    { // backward compatibility
-      path: '/:network(main|babylon|zero|carthage)/:address(KT[0-9A-z]{34})',
-      children: [
-        {
-          path: '',
-          redirect: to => {
-            const { params } = to
-            return `/${params.network}net/${params.address}`
-          }
-        },
-        {
-          path: 'operations',
-          redirect: to => {
-            const { params } = to
-            return `/${params.network}net/${params.address}/operations`
-          }
-        },
-        {
-          path: 'script',
-          redirect: to => {
-            const { params } = to
-            return `/${params.network}net/${params.address}/code`
-          }
-        },
-        {
-          path: 'state',
-          redirect: to => {
-            const { params } = to
-            return `/${params.network}net/${params.address}/storage`
-          }
-        },
-      ]
-    },
-    {
-      path: '/:network/:address([0-9A-z]{36})',
-      components: {
-        default: Contract,
-      },
-      props: { default: true },
-      children: [
-        {
-          path: '',
-          name: 'contract',
-          redirect: 'operations'
-        },
-        {
-          path: 'operations',
-          name: 'operations',
-          component: OperationsTab,
-          props: true
-        }, 
-        {
-          path: 'events',
-          name: 'events',
-          component: EventsTab,
-          props: true
-        },
-        {
-          path: 'interact/:entrypoint?',
-          name: 'interact',
-          component: InteractTab,
-          props: true
-        },
-        {
-          path: 'views/:view?',
-          name: 'views',
-          component: ViewsTab,
-          props: true
-        },
-        {
-          path: 'storage',
-          name: 'storage',
-          component: StorageTab,
-          props: true,
-          children: [
-            {
-              path: 'big_map/:ptr(\\d+)',
-              components: {
-                default: BigMap
-              },
-              props: { default: true },
-              children: [
-                {
-                  path: '',
-                  name: 'big_map',
-                  redirect: 'keys'
-                },
-                {
-                  path: 'keys',
-                  name: 'big_map_keys',
-                  component: BigMapKeys,
-                  props: true
-                },
-                {
-                  path: ':keyhash',
-                  name: 'big_map_history',
-                  component: BigMapHistory,
-                  props: true
-                }
-              ]
-            },
-          ]
-        },
-        {
-          path: 'code',
-          name: 'code',
-          component: CodeTab,
-          props: true
-        },
-        {
-          path: 'metadata',
-          name: 'metadata',
-          component: MetadataTab,
-          props: true
-        },
-        {
-          path: 'fork',
-          name: 'fork',
-          components: {
-            default: ForkTab,
-          },
-          props: true,
-        },
-        {
-          path: 'tokens',
-          name: 'tokens',
-          component: ContractTokensTab,
-          props: true
-        },
-        {
-          path: 'stats',
-          name: 'contract_stats',
-          component: ContractStatsTab,
-          props: true
-        },
-        {
-          path: 'details',
-          name: 'details',
-          component: DetailsTab,
-          props: true
-        },
-        {
-          path: 'transfers',
-          name: 'transfers',
-          component: TransfersTab,
-          props: true
-        },
-      ]
-    },
-    {
-      path: '/:network/opg/:hash(o[0-9A-z]{50})',
-      alias: '/:network(main|babylon|zero|carthage)/:hash(o[0-9A-z]{50})',
-      components: {
-        default: OperationGroup
-      },
-      props: { default: true },
-      children: [
-        {
-          path: '',
-          name: 'operation_group',
-          redirect: 'contents'
-        },
-        {
-          path: 'contents',
-          name: 'opg_contents',
-          component: OpgContents,
-          props: true
-        }
-      ]
-    }
-  ]
-});
+function validateNetwork(networks) {
+  return function(to, _from, next) {
+    if (networks.indexOf(to.params.network) == -1)
+      next('/not_found');
+    else
+      next()
+  }
+}
 
-export default router;
+export function newRouter(networks) {
+  return new Router({
+    linkActiveClass: '',
+    linkExactActiveClass: '',
+    mode: 'history',
+    scrollBehavior () {
+      window.scrollTo(0,0)
+    },
+    routes: [
+      {
+        path: '/',
+        components: {
+          default: Home
+        },
+        name: 'home'
+      },
+      {
+        path: '/not_found',
+        components: {
+          default: NotFound,
+        },
+        name: 'not_found',
+      },
+      {
+        path: '/search',
+        components: {
+          default: ExtendedSearch
+        },
+        name: 'search',
+        props: { default: true },
+      },
+      {
+        path: '/dapps*',
+        props: { default: true },
+        redirect() {
+          window.location.href = 'https://tzkt.io/dapps'
+        }
+      },
+      {
+        path: '/deploy',
+        components: {
+          default: Deploy,
+        },
+        name: 'deploy',
+        props: { default: true },
+      },
+      {
+        path: '/docs',
+        components: {
+          default: Docs
+        },
+        name: 'docs',
+        props: { default: true },
+      },
+      {
+        path: '/constant/:network/:address',
+        beforeEnter: validateNetwork(networks),
+        components: {
+          default: Constant,
+        },
+        props: { default: true },
+        children: [
+          {
+            path: '',
+            name: 'constant',
+            component: ConstantTab,
+            props: true,
+          },
+        ]
+      },
+      {
+        path: '/:network',
+        beforeEnter: validateNetwork(networks),
+        components: {
+          default: Network,
+        },
+        props: { default: true },
+        children: [
+          {
+            path: '',
+            name: 'network',
+            redirect: 'recently_called'
+          },{
+            path: 'recently_called',
+            name: 'recently_called',
+            component: RecentlyCalledTab,
+            props: true,
+          },{
+            path: 'constants',
+            name: 'network_constants',
+            component: ConstantsTab,
+            props: true,
+          },{
+            path: 'stats',
+            name: 'network_stats',
+            component: StatsTab,
+            props: true,
+          },
+        ]
+      },
+      {
+        path: '/:network/:address([0-9A-z]{36})',
+        components: {
+          default: Contract,
+        },
+        props: { default: true },
+        children: [
+          {
+            path: '',
+            name: 'contract',
+            redirect: 'operations'
+          },
+          {
+            path: 'operations',
+            name: 'operations',
+            component: OperationsTab,
+            props: true
+          },
+          {
+            path: 'events',
+            name: 'events',
+            component: EventsTab,
+            props: true
+          },
+          {
+            path: 'interact/:entrypoint?',
+            name: 'interact',
+            component: InteractTab,
+            props: true
+          },
+          {
+            path: 'views/:view?',
+            name: 'views',
+            component: ViewsTab,
+            props: true
+          },
+          {
+            path: 'storage',
+            name: 'storage',
+            component: StorageTab,
+            props: true,
+            children: [
+              {
+                path: 'big_map/:ptr(\\d+)',
+                components: {
+                  default: BigMap
+                },
+                props: { default: true },
+                children: [
+                  {
+                    path: '',
+                    name: 'big_map',
+                    redirect: 'keys'
+                  },
+                  {
+                    path: 'keys',
+                    name: 'big_map_keys',
+                    component: BigMapKeys,
+                    props: true
+                  },
+                  {
+                    path: ':keyhash',
+                    name: 'big_map_history',
+                    component: BigMapHistory,
+                    props: true
+                  }
+                ]
+              },
+            ]
+          },
+          {
+            path: 'code',
+            name: 'code',
+            component: CodeTab,
+            props: true
+          },
+          {
+            path: 'metadata',
+            name: 'metadata',
+            component: MetadataTab,
+            props: true
+          },
+          {
+            path: 'fork',
+            name: 'fork',
+            components: {
+              default: ForkTab,
+            },
+            props: true,
+          },
+          {
+            path: 'tokens',
+            name: 'tokens',
+            component: ContractTokensTab,
+            props: true
+          },
+          {
+            path: 'stats',
+            name: 'contract_stats',
+            component: ContractStatsTab,
+            props: true
+          },
+          {
+            path: 'details',
+            name: 'details',
+            component: DetailsTab,
+            props: true
+          },
+          {
+            path: 'transfers',
+            name: 'transfers',
+            component: TransfersTab,
+            props: true
+          },
+        ]
+      },
+      {
+        path: '/:network(mainnet|ghostnet|jakartanet|kathmandunet|sandboxnet)/opg/:hash(o[0-9A-z]{50})',
+        alias: '/:network(mainnet|ghostnet|jakartanet|kathmandunet|sandboxnet)/:hash(o[0-9A-z]{50})',
+        components: {
+          default: OperationGroup
+        },
+        props: { default: true },
+        children: [
+          {
+            path: '',
+            name: 'operation_group',
+            redirect: 'contents'
+          },
+          {
+            path: 'contents',
+            name: 'opg_contents',
+            component: OpgContents,
+            props: true
+          }
+        ]
+      }
+    ]
+  });
+}
