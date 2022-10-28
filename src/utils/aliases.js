bcd9cd8f033e02e9fc2c72811e55b6115d239319
Artem <aopoltorzhicky@gmail.com>
2022-10-28 15:59:38 +0400

    Fix: expiration aliases cache

--- src/utils/aliases.js
+++ src/utils/aliases.js
@@ -1,9 +1,12 @@
 const aliasesKey = 'aliases';
+const expiryKey = 'expiry_aliases'
 
 export class Aliases {
     constructor(size) {
         this.size = size;
         this.aliases = JSON.parse(localStorage.getItem(aliasesKey)) || {};
+
+        this.setExpiryKey();
     }
 
     add(key, alias) {
@@ -11,12 +14,36 @@ export class Aliases {
         if (keys.length === this.size) {
             delete this.aliases[keys[0]]
         }
-        this.aliases[key] = alias;
+
+        this.checkExpirationTime();
+        this.aliases[key] = alias;        
         localStorage.setItem(aliasesKey, JSON.stringify(this.aliases));
     }
 
     get(key) {
+        this.checkExpirationTime();
         return this.aliases[key];
+    }
+
+    setExpiryKey() {
+        const expiryString = localStorage.getItem(expiryKey);
+        if (!expiryString) {
+            this.expiry = new Date().getTime() + 24 * 60 * 60 * 1000;
+            localStorage.setItem(expiryKey, this.expiry);
+        } else {
+            this.expiry = parseInt(expiryString, 10);
+        }
+    }
+
+    checkExpirationTime() {
+        const now = new Date().getTime();
+        console.log(now, this.expiry)
+        if (now > this.expiry) {
+            this.aliases = {};
+            localStorage.removeItem(aliasesKey);
+            localStorage.removeItem(expiryKey);
+            this.setExpiryKey();
+        }
     }
 }
 
