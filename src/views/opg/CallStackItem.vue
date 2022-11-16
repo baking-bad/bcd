022162c9285a0617e46cb692cc6a17e387e0d047
Artem <aopoltorzhicky@gmail.com>
2022-11-16 18:07:34 +0400

    Fix: call stack aliases

---
+++ src/views/opg/CallStackItem.vue
@@ -0,0 +1,32 @@
+<template>
+    <div class="d-flex align-center" :class="operation.internal ? 'mt-2' : 'mt-4'">
+        <span v-if="operation.internal" class="mr-2" style="font-size: 14px">↳</span>
+        <div>
+        <span class="text--secondary" v-if="alias">{{ alias }}</span>
+        <Shortcut v-else class="text--secondary" :str="operation.destination"/>
+        <span class="text--secondary" style="font-size: 20px">→</span>
+        <span v-if="operation.entrypoint" class="hash">{{ operation.entrypoint }}()</span>
+        <span v-else>{{ operation.amount | uxtz }}</span>
+        </div>
+    </div>
+</template>
+
+<script>
+import Shortcut from "@/components/Shortcut.vue";
+
+export default {
+    name: "CallStackItem",
+    props: {
+        network: String,
+        operation: Object
+    },
+    components: {
+        Shortcut
+    },
+    computed: {
+        alias() {
+            return this.getAlias(this.network, this.operation.destination)
+        }
+    }
+}
+</script>
