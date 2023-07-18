276afcf389b5f47ab3dbd00340d6db6f7cb6607b
Artem Poltorzhitskiy <aopoltorzhicky@gmail.com>
2023-07-18 19:05:46 +0200

    Fix: check Destination field in operation view on search page (#542)

--- src/views/extended_search/result/Operation.vue
+++ src/views/extended_search/result/Operation.vue
@@ -6,7 +6,7 @@
                     <v-list-item-title>
                         <span class="hash">Operation</span>
                         <span class="text--secondary" style="font-size: 20px;"> → </span>
-                        <template v-if="item.body.Destination.startsWith('KT')">
+                        <template v-if="isDestinationContract">
                             <router-link class="serp-link" target="_blank" :to="`/${item.body.Network}/${item.body.Destination}`">
                                 <Shortcut :str="item.body.Destination"/>
                             </router-link>
@@ -15,7 +15,7 @@
                         <router-link class="serp-link" target="_blank" :to="link">
                             <span v-if="item.body.Entrypoint" class="hash">{{ item.body.Entrypoint + '()' }}</span>
                             <span v-else-if="item.body.Type === 'origination'">origination</span>
-                            <Shortcut v-else-if="item.body.Destination.startsWith('KT')" :str="item.body.Hash"/>
+                            <span v-else-if="isDestinationContract">default()</span>
                             <span v-else class="hash">{{ item.body.Hash }}</span>
                         </router-link>
                     </v-list-item-title>
@@ -34,6 +34,7 @@
 <script>
 import Highlight from './Highlight.vue';
 import Shortcut from '@/components/Shortcut.vue';
+import {isKT1Address} from "@/utils/tz";
 
 export default {
     name: "Operation",
@@ -57,6 +58,12 @@ export default {
                 return `/${this.item.body.Network}/opg/${this.item.body.Hash}`;
             }
             return '';
+        },
+        isDestinationContract() {
+            if (this.item.body.Destination){
+                return isKT1Address(this.item.body.Destination)
+            }
+            return false;
         }
     }
 }
