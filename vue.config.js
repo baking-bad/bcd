357c5030e8a12fc02ae78a316487a74daa329ac7
sstark21 <va.sabitov@gmail.com>
2025-07-29 15:43:42 +0100

    upg beacon

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,17 @@
+const NodePolyfillPlugin = require('node-polyfill-webpack-plugin');
+const webpack = require('webpack');
+
 module.exports = {
   "transpileDependencies": [
     "vuetify"
-  ]
+  ],
+  configureWebpack: {
+    plugins: [
+      new NodePolyfillPlugin(),
+      new webpack.ProvidePlugin({
+        Buffer: ['buffer', 'Buffer'],
+        process: 'process/browser'
+      })
+    ]
+  }
 }
